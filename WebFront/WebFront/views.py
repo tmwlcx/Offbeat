082d49ed085fb82b35743fa337f847883937aeb2
Inness, James Tyler <james.inness@gatech.edu>
2020-03-02 08:21:52 -0500

    add json import

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -1,6 +1,8 @@
 from django.http import HttpResponse
 import pandas as pd
 import pymysql
+# transform data to json for sending to front end when using d3
+import json
 
 def shout(request):
     conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY') 
