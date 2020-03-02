f61d23a75cc7a374868de60bde1f46be1f06e9b4
Inness, James Tyler <james.inness@gatech.edu>
2020-03-02 08:14:37 -0500

    update shout to return results

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -1,26 +1,10 @@
 from django.http import HttpResponse
 import pandas as pd
-import sqlalchemy
+import pymysql
 
 def shout(request):
-    cloud_sql_connection_name = 'propane-ground-269323:us-east1:project-sql-space'
+    db = pymysql.connections.Connection('127.0.0.1', user='TeamEleven', password='dbpassword', database='SPOTIFY')  
 
-    # The SQLAlchemy engine will help manage interactions, including automatically
-    # managing a pool of connections to your database
-    db = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL(
-            drivername="mysql+pymysql",
-            username='Team',
-            password='Projectime',
-            database='The_Database',
-            query={"unix_socket": "/cloudsql/{}".format(cloud_sql_connection_name)},
-        ),
-        # ... Specify additional properties here.
-        # ...
-    )
+    artists = db.execute("select * from Artists LIMIT 10;")
 
-    schem = db.execute("select * from schema;")
-
-    return HttpResponse('This page is HERE: {}'.format(schem))
+    return render(request, 'home.html', context={'table' : artists})
