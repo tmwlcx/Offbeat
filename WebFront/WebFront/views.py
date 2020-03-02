276b74f136724328667034c63da45624e23b4b48
Inness, James Tyler <james.inness@gatech.edu>
2020-03-02 08:44:26 -0500

    typo in table

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -7,6 +7,6 @@ import json
 def shout(request):
     conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY') 
 
-    artists = conn.execute("select * from Artists LIMIT 10;")
+    artists = conn.execute("select * from artist LIMIT 10;")
 
     return render(request, 'home.html', context={'table' : artists})
