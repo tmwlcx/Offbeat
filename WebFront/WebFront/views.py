c8014042b2945bb92e80d4bd1bbbc304131e11cc
Inness, James Tyler <james.inness@gatech.edu>
2020-03-03 08:45:25 -0500

    try app connection

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -5,7 +5,23 @@ import pymysql
 import json
 
 def shout(request):
-    conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY') 
+    
+    # The SQLAlchemy engine will help manage interactions, including automatically
+    # managing a pool of connections to your database
+    conn = sqlalchemy.create_engine(
+        # Equivalent URL:
+        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+        sqlalchemy.engine.url.URL(
+            drivername="mysql+pymysql",
+            username='teameleven',
+            password='dbpassword',
+            database='SPOTIFY',
+            query={"unix_socket": "/cloudsql/{}".format('propane-ground-269323:us-east1:spotify-instance')},
+        ),
+        # ... Specify additional properties here.
+        # ...
+    )
+    #conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY') 
 
     cur = conn.cursor()
 
