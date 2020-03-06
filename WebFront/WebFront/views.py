f1bc28f0bf59398cf647e013ca34747d64f5a527
Inness, James Tyler <james.inness@gatech.edu>
2020-03-06 09:28:51 -0500

    testing Queries

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -9,28 +9,44 @@ def shout(request):
     
     # The SQLAlchemy engine will help manage interactions, including automatically
     # managing a pool of connections to your database
-    conn = sqlalchemy.create_engine(
-        # Equivalent URL:
-        # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
-        sqlalchemy.engine.url.URL(
-            drivername="mysql+pymysql",
-            username='teameleven',
-            password='dbpassword',
-            database='SPOTIFY',
-            query={"unix_socket": "/cloudsql/{}".format('propane-ground-269323:us-east1:spotify-instance')},
-        ),
-        # ... Specify additional properties here.
-        # ...
-    )
-    #conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY') 
+    try:
+        conn = sqlalchemy.create_engine(
+            # Equivalent URL:
+            # mysql+pymysql://<db_user>:<db_pass>@/<db_name>?unix_socket=/cloudsql/<cloud_sql_instance_name>
+            sqlalchemy.engine.url.URL(
+                drivername="mysql+pymysql",
+                username='teameleven',
+                password='dbpassword',
+                database='SPOTIFY',
+                query={"unix_socket": "/cloudsql/{}".format('propane-ground-269323:us-east1:spotify-instance')},
+            ),
+            # ... Specify additional properties here.
+            # ...
+        )
+        cur = conn.cursor()
 
-    cur = conn.cursor()
+        sql = "select * from artist LIMIT 10;"
 
-    sql = "select * from artist LIMIT 10;"
+        cur.execute(sql)
 
-    cur.execute(sql)
-    
-    artists = cur.fetchall()
-    conn.commit()
+        artists = cur.fetchall()
+        conn.commit()
+        
+    except:
+        b = "Item one failed"
+        try:
+            conn = pymysql.connect('35.196.88.209', 'teameleven', 'dbpassword', 'SPOTIFY')
+            cur = conn.cursor()
+
+            sql = "select * from artist LIMIT 10;"
+
+            cur.execute(sql)
+
+            artists = cur.fetchall()
+            conn.commit()
+        except:
+            b = "Item two failed"
+            artists = [a, b]
+
 
     return render(request, 'home.html', context={'table' : artists})
