f3c8f476912f1fff0d463874430de61d0bdad5e6
Rose, Jeffrey Charles <jrose67@gatech.edu>
2020-04-06 18:43:13 -0400

    can't push decompressed file to gcloud.

--- Backend/deploy.sh
+++ Backend/deploy.sh
@@ -4,8 +4,6 @@ rm -rf cse6242-2020-TeamProject/
 
 git clone https://github.gatech.edu/jinness3/cse6242-2020-TeamProject.git
 
-gzip -d /home/teamelevenproject/cse6242-2020-TeamProject/cse6242-2020-TeamProject/WebFront/static/center_distances_final.csv.gz
-
 cd /home/teamelevenproject/cse6242-2020-TeamProject/cse6242-2020-TeamProject/WebFront
 
 gcloud app deploy
