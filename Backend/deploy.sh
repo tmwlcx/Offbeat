5ec807fa47d3f726805c8205a6bbc9155e087f08
Rose, Jeffrey Charles <jrose67@gatech.edu>
2020-04-06 18:24:32 -0400

    bug fix

--- Backend/deploy.sh
+++ Backend/deploy.sh
@@ -4,6 +4,6 @@ rm -rf cse6242-2020-TeamProject/
 
 git clone https://github.gatech.edu/jinness3/cse6242-2020-TeamProject.git
 
-cd /home/teamelevenproject/cse6242-2020-TeamProject/cse6242-2020-TeamProject/WebFront
+cd /home/teamelevenproject/cse6242-2020-TeamProject/WebFront
 
 gcloud app deploy
