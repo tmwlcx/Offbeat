b0afdda2300fef35ee0465a432f33b1651ed072b
Rose, Jeffrey Charles <jrose67@gatech.edu>
2020-04-06 18:31:44 -0400

    logic changes

--- Backend/deploy.sh
+++ Backend/deploy.sh
@@ -1,9 +1,11 @@
-cd ~
+cd ~/cse6242-2020-TeamProject/
 
 rm -rf cse6242-2020-TeamProject/
 
 git clone https://github.gatech.edu/jinness3/cse6242-2020-TeamProject.git
 
-cd /home/teamelevenproject/cse6242-2020-TeamProject/WebFront
+gzip -d /home/teamelevenproject/cse6242-2020-TeamProject/cse6242-2020-TeamProject/WebFront/static/center_distances_final.csv.gz
+
+cd /home/teamelevenproject/cse6242-2020-TeamProject/cse6242-2020-TeamProject/WebFront
 
 gcloud app deploy
