0473ad36bf2f1a6431e52d2a0091fa3e5bab5a17
JTInness <36108888+JTInness@users.noreply.github.com>
2020-04-11 18:57:30 -0400

    update js

--- WebFront/static/script.js
+++ WebFront/static/script.js
@@ -76,59 +76,73 @@ $.ajax({
             }
           });
 
-          var danceability_avg = 0;
-          var energy_avg = 0;
-          var loudness_avg = 0;
-          var speechiness_avg = 0;
-          var acousticness_avg = 0;
-          var liveness_avg = 0;
-          var valence_avg = 0;
-          var tempo_avg = 0;
+          var danceability_total = 0;
+          var energy_total = 0;
+          var speechiness_total = 0;
+          var acousticness_total = 0;
+          var liveness_total = 0;
+          var valence_total = 0;
+          var loudness_total = 0;
+          var tempo_total = 0;
 
           //console.log(songs);
-          for (i=0; i<songs.length; i++) {
-            danceability_avg += songs[i].danceability;
-            energy_avg += songs[i].energy;
-            loudness_avg += songs[i].loudness;
-            speechiness_avg += songs[i].speechiness;
-            acousticness_avg += songs[i].acousticness;
-            liveness_avg += songs[i].liveness;
-            valence_avg += songs[i].valence;
-            tempo_avg += songs[i].tempo;
+          for (i = 0; i < songs.length; i++) {
+              danceability_total += songs[i].danceability;
+              energy_total += songs[i].energy;
+              loudness_total += songs[i].loudness;
+              speechiness_total += songs[i].speechiness;
+              acousticness_total += songs[i].acousticness;
+              valence_total += songs[i].valence;
+              liveness_total += songs[i].liveness;
+              tempo_total += songs[i].tempo;
           }
 
           var user_averages = {
-            'danceability': danceability_avg/num_songs,
-            'energy': energy_avg / num_songs,
-            'loudness': loudness_avg / num_songs,
-            'speechiness': speechiness_avg/num_songs,
-            'acousticness': acousticness_avg/num_songs,
-            'liveness': liveness_avg/num_songs,
-            'valence': valence_avg/num_songs,
-            'tempo': tempo_avg/num_songs
+              'how_offbeat': 10,
+              'audio_features': {
+                  'danceability': danceability_total / num_songs,
+                  'energy': energy_total / num_songs,
+                  'speechiness': speechiness_total / num_songs,
+                  'acousticness': acousticness_total / num_songs,
+                  'liveness': liveness_total / num_songs,
+                  'valence': valence_total / num_songs,
+                  'loudness': loudness_total / num_songs,
+                  'tempo': tempo_total / num_songs
+              }
           };
 
           post_object = {
               "Values": {
-                  "danceability": danceability_avg/num_songs,
-                  "energy": energy_avg / num_songs,
-                  "loudness": loudness_avg / num_songs,
-                  "speechiness": speechiness_avg / num_songs,
-                  "acousticness": acousticness_avg / num_songs,
-                  "liveness": liveness_avg / num_songs,
-                  "valence": valence_avg / num_songs,
-                  "tempo": tempo_avg / num_songs
+                  "danceability": danceability_total / num_songs,
+                  "energy": energy_total / num_songs,
+                  "loudness": loudness_total / num_songs,
+                  "speechiness": speechiness_total / num_songs,
+                  "acousticness": acousticness_total / num_songs,
+                  "liveness": liveness_total / num_songs,
+                  "valence": valence_total / num_songs,
+                  "tempo": tempo_total / num_songs
               }
           }
 
-          danceability_avg = Math.round(danceability_avg/num_songs * 100);
-          energy_avg = Math.round(energy_avg / num_songs * 100);
-          loudness_avg = Math.round(loudness_avg / num_songs * 100);
-          speechiness_avg = Math.round(speechiness_avg/num_songs * 100);
-          acousticness_avg = Math.round(acousticness_avg/num_songs * 100);
-          liveness_avg = Math.round(liveness_avg/num_songs * 100);
-          valence_avg = Math.round(valence_avg/num_songs * 100);
-          tempo_avg = Math.round(tempo_avg/num_songs);
+          var features_total = [
+            danceability_total,
+            energy_total,
+            speechiness_total,
+            acousticness_total,
+            liveness_total,
+            valence_total,
+            loudness_total,
+            tempo_total
+          ];
+
+          danceability_avg = Math.round(danceability_total / num_songs * 100);
+          energy_avg = Math.round(energy_total / num_songs * 100);
+          speechiness_avg = Math.round(speechiness_total / num_songs * 100);
+          acousticness_avg = Math.round(acousticness_total / num_songs * 100);
+          liveness_avg = Math.round(liveness_total / num_songs * 100);
+          valence_avg = Math.round(valence_total / num_songs * 100);
+          loudness_avg = Math.round(loudness_total / num_songs);
+          tempo_avg = Math.round(tempo_total / num_songs);
 
           //
           let structure_html = $('<div class="row" id="profile-row"><div class="col-md-3" id="profile-row-left"></div><div class="col-md-9" id="profile-row-right"></div></div>');
@@ -136,26 +150,114 @@ $.ajax({
           let average_text = $('<p>Your listening profile:</p>');
           average_text.appendTo($('#profile-row-left'));
 
-          $('<ul></ul>').appendTo('#profile-row-left');
-          $('<li>danceability: ' + danceability_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>energy: ' + energy_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>loudness: ' + loudness_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>speechiness: ' + speechiness_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>acousticness: ' + acousticness_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>liveness: ' + liveness_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>valence: ' + valence_avg + '%</li>').appendTo('#profile-row-left ul');
-          $('<li>tempo: ' + tempo_avg + 'bpm</li>').appendTo('#profile-row-left ul');
+            // html for profile section
+          let profile_section = $('<div id="profile_rect"></div>');
+          profile_section.appendTo($('#profile-row-left'));
 
+            // initial svg for profile section
+          var profile_svg = d3.select('#profile_rect')
+            .append('svg')
+            .attr('height', 370)
+            .attr('width', 360);
 
-          let offbeat_text = $('<p>How offbeat do you want to get?</p>');
+            // vertical lines
+          profile_svg.append('line')
+            .attr('stroke', '#d4d4d4')
+            .attr('stroke-width', 1)
+            .attr('x1', 130)
+            .attr('x2', 130)
+            .attr('y1', 0)
+            .attr('y2', 361);
+
+            // feature rects
+          var rect_color = 'rgba(30, 215, 96, 0.5)';
+          for (var i = 0; i < features_total.length - 2; i++) {
+              // rect
+              profile_svg.append('rect')
+                .attr('fill', rect_color)
+                .attr('height', 35)
+                .attr('width', 200 * (features_total[i] / num_songs))
+                .attr('x', 130)
+                .attr('y', 7 + (i * 45));
+              // text
+              profile_svg.append('text')
+                .text(features_total[i])
+                .attr('class', 'audio_feature')
+                .attr('font-size', '14px')
+                .attr('fill', '#717171')
+                .attr('x', 123)
+                .attr('text-anchor', 'end')
+                .attr('y', 30 + (i * 45))
+              profile_svg.append('text')
+                .text(Math.round(features_total[i] / num_songs * 100) + '%')
+                .attr('font-size', '14px')
+                .attr('fill', '#717171')
+                .attr('x', 130 + (200 * (features_total[i] / num_songs) + 5))
+                .attr('y', 30 + (i * 45))
+          };
+
+            // loudness text
+          profile_svg.append('text')
+            .text('loudness') //+ Math.round(tempo_total/num_songs) + 'bpm')
+            .attr('class', 'audio_feature')
+            .attr('font-size', '14px')
+            .attr('fill', '#717171')
+            .attr('x', 123)
+            .attr('text-anchor', 'end')
+            .attr('y', 297);
+          profile_svg.append('text')
+            .text(Math.round(loudness_total / num_songs) + ' dB')
+            .attr('font-size', '14px')
+            .attr('fill', '#717171')
+            .attr('x', 135)
+            .attr('y', 297);
+
+            // tempo text
+          profile_svg.append('text')
+            .text('tempo') //+ Math.round(tempo_total/num_songs) + 'bpm')
+            .attr('class', 'audio_feature')
+            .attr('font-size', '14px')
+            .attr('fill', '#717171')
+            .attr('x', 123)
+            .attr('text-anchor', 'end')
+            .attr('y', 342);
+          profile_svg.append('text')
+            .text(Math.round(tempo_total / num_songs) + 'bpm')
+            .attr('font-size', '14px')
+            .attr('fill', '#717171')
+            .attr('x', 135)
+            .attr('y', 342);
+
+            // tooltips
+          var tip = d3.tip()
+            .attr('class', 'd3-tip')
+            .offset([-10, 0])
+            .html(function (d) {
+                var text_hovered = d3.select(this).text();
+                return text_hovered.startsWith('danceability') ? 'Danceability describes how suitable a track is for dancing based on a combination of musical elements including tempo, rhythm stability, beat strength, and overall regularity.'
+                     : text_hovered.startsWith('energy') ? 'Energy represents a perceptual measure of intensity and activity. Typically, energetic tracks feel fast, loud, and noisy.'
+                     : text_hovered.startsWith('speechiness') ? 'Speechiness detects the presence of spoken words in a track. The more exclusively speech-like the recording (e.g. talk show, audio book, poetry), the higher the value.'
+                     : text_hovered.startsWith('acousticness') ? 'A confidence measure of whether the track is acoustic.'
+                     : text_hovered.startsWith('liveness') ? 'Detects the presence of an audience in the recording. Higher liveness values represent an increased probability that the track was performed live.'
+                     : text_hovered.startsWith('valence') ? 'A measure describing the musical positiveness conveyed by a track. Tracks with high valence sound more positive (e.g. happy, cheerful, euphoric), while tracks with low valence sound more negative (e.g. sad, depressed, angry).'
+                     : text_hovered.startsWith('loudness') ? 'The overall loudness of a track in decibels (dB). Loudness is the quality of a sound that is the primary psychological correlate of physical strength (amplitude). Values typical range between -60 and 0 db.'
+                     : text_hovered.startsWith('tempo') ? 'The overall estimated tempo of a track in beats per minute (BPM). In musical terminology, tempo is the speed or pace of a given piece and derives directly from the average beat duration.'
+                     : '';
+            });
+          profile_svg.call(tip);
+          d3.selectAll('#profile_rect text.audio_feature')
+            .on('mouseover', tip.show)
+            .on('mouseout', tip.hide)
+
+            // final question & button
+          let offbeat_question = $('<p style="margin-bottom: 0px;">How offbeat do you want to get?</p>');
+          let offbeat_question_subtext = $('<p style="font-size:12px; color:#ababab;">Choose the degree to which the recommendations will be different from the music you usually listen to (higher: more different, lower: less different).</p>');
           let slider = $('<div class="sliders"><div id="slider-offbeat"></div></div>');
-          let explore_button = $('<button onclick="return_results()">Explore</button>');
-          let user_avg_text = $('<hr></hr><p>[for testing] example of JSON that could be sent to the back end</p>');
-          let user_avg_json = $('<pre>' + JSON.stringify(user_averages, undefined, 2) + '</pre>');
-          let top_50_json_text = $('<p>[for testing] audio feature data returned for your top ' + num_songs + ' songs from API endpoints:</p>');
-          let top_50_json_viewable = $('<pre>' + JSON.stringify(songs, undefined, 2) + '</pre>');
+          let explore_button = $('<button onclick="drawGraph()">Explore</button>');
+          let playback = $('<iframe src="https://open.spotify.com/embed/track/7AttuQ5rWnfYlvHgP8OcGN" width="300" height="80" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe>');
 
-          offbeat_text.appendTo($('#profile-row-left'));
+          offbeat_question.appendTo($('#profile-row-left'));
+          offbeat_question_subtext.appendTo($('#profile-row-left'));
           slider.appendTo($('#profile-row-left'));
 
 
@@ -176,11 +278,20 @@ $.ajax({
           // final button to draw the svg
           explore_button.appendTo($('#profile-row-left'));
 
+          // draw the legend
+          let legend = $('<svg id="mylegend" width="700" height="60"></svg>');
+          legend.appendTo($('#profile-row-right'));
+
           // draw the svg
           let circles = $('<svg id="mychart" width="700" height="700"></svg>');
           circles.appendTo($('#profile-row-right'));
 
+
           // print consolidated json data at the bottom of the page
+          let user_avg_text = $('<hr></hr><p>[for testing] example of JSON that could be sent to the back end:</p>');
+          let user_avg_json = $('<pre>' + JSON.stringify(user_averages, undefined, 2) + '</pre>');
+          let top_50_json_text = $('<p>[for testing] audio feature data returned for your top ' + num_songs + ' songs from API endpoints:</p>');
+          let top_50_json_viewable = $('<pre>' + JSON.stringify(songs, undefined, 2) + '</pre>');
           user_avg_text.appendTo($('#testing'));
           user_avg_json.appendTo($('#testing'));
           top_50_json_text.appendTo($('#testing'));
@@ -208,7 +319,79 @@ return_results = function () {
 
 function drawGraph(res_data) {
 
-    console.log(res_data);
+    // clear the legend and all of the circle svg's child elements on redraw
+    d3.select("#mylegend g").remove()
+    d3.select("#mychart g").remove()
+
+    // draw the legend & text
+    var cScale = d3.scaleSequential(d3.interpolateBlues)
+      .domain([0, 99]);
+
+    var xScale = d3.scaleLinear()
+        .domain([0, 99])
+        .range([0, 700]);
+
+    var svg_legend = d3.select("#mylegend")
+      .append("g")
+    var legend_scale = d3.select("#mylegend")
+      .selectAll("rect")
+      .data(Array.from(Array(100).keys()))
+      .enter().append("rect")
+      .attr("x", (d) => Math.floor(xScale(d)))
+      .attr("y", 0)
+      .attr("width", (d) => {
+          if (d == 99) { return 6; }
+          else { return Math.floor(xScale(d + 1)) - Math.floor(xScale(d)) + 1; }
+      })
+      .attr("height", 25)
+      .attr("fill", (d) => cScale(d));
+
+    svg_legend.append("text")
+      .attr("x", 0)
+      .attr("y", 38)
+      .text("far away from what")
+      .style("font-size", "12px")
+      .style("fill", "#ababab");
+    svg_legend.append("text")
+      .attr("x", 700)
+      .attr("y", 38)
+      .text("very close to what")
+      .style("font-size", "12px")
+      .style("fill", "#ababab")
+      .attr("text-anchor", "end");
+    svg_legend.append("text")
+      .attr("x", 0)
+      .attr("y", 50)
+      .text("you asked for")
+      .style("font-size", "12px")
+      .style("fill", "#ababab");
+    svg_legend.append("text")
+      .attr("x", 700)
+      .attr("y", 50)
+      .text("you asked for")
+      .style("font-size", "12px")
+      .style("fill", "#ababab")
+      .attr("text-anchor", "end");
+
+    // draw the graph
+    var circles_svg = d3.select("#mychart"),
+      margin = 20,
+      diameter = +circles_svg.attr("width"),
+      g = circles_svg.append("g")
+        .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")")
+        .attr("id", "mychart-g");
+
+    var color = d3.scaleLinear()
+      .domain([-1, 5]) // for depths
+      .range(["hsl(0,0%,97%)", "hsl(0,0%,50%)"]) // greys
+      .interpolate(d3.interpolateHcl);
+
+    var colorScale = d3.scaleSequential(d3.interpolateBlues) // for nodes at the very bottomn
+      .domain([0, 1])
+
+    var pack = d3.pack()
+      .size([diameter - margin, diameter - margin])
+      .padding(2);
 
     // draw the graph
     var svg = d3.select("#mychart"),
