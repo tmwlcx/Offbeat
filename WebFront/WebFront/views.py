e71d713680bfceee195897d4e1fc5ecee2320f96
Inness, James Tyler <james.inness@gatech.edu>
2020-04-14 11:43:22 -0400

    fix shape of curr_cluster

--- WebFront/WebFront/views.py
+++ WebFront/WebFront/views.py
@@ -178,7 +178,7 @@ def Path_to_Data(request):
 	for val in All_Level_3_Centers['centroid_id']:
 		nested_data = {}
 		nested_data["name"] = int(val)
-		curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(val)].values.flatten().tolist()[1:]).reshape(-1,1)])
+		curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(val)].values.flatten().tolist()[1:]).reshape(1,-1)])
 		distance = orig_cluster_transform - curr_cluster
 		dist_indexes = list(np.argsort(np.abs(distance)).reshape(1,-1).flatten())[0:3]
 		names = [feature_Order[i] for i in dist_indexes]
@@ -190,7 +190,7 @@ def Path_to_Data(request):
 		for val1 in set(children[children['level3']==val]['level2']):
 			temp = {}
 			temp['name'] = int(val1)
-			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(-1,1)])
+			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(1,-1)])
 			distance = orig_cluster_transform - curr_cluster
 			dist_indexes = list(np.argsort(np.abs(distance)).reshape(1,-1).flatten())[0:3]
 			names = [feature_Order[i] for i in dist_indexes]
@@ -207,7 +207,7 @@ def Path_to_Data(request):
 		for discard, song_vals1 in next_level.iterrows():
 			temp = {}
 			temp['name'] = int(song_vals1['level1'])
-			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(-1,1)])
+			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(1,-1)])
 			distance = orig_cluster_transform - curr_cluster
 			dist_indexes = list(np.argsort(np.abs(distance)).reshape(1,-1).flatten())[0:3]
 			names = [feature_Order[i] for i in dist_indexes]
@@ -224,7 +224,7 @@ def Path_to_Data(request):
 		for discard, song_vals1 in next_level.iterrows():
 			temp = {}
 			temp['name'] = int(song_vals1['level0'])
-			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(-1,1)])
+			curr_cluster = np.array([float(x) for x in np.array(centers[centers["centroid_id"]==int(temp["name"])].values.flatten().tolist()[1:]).reshape(1,-1)])
 			distance = orig_cluster_transform - curr_cluster
 			dist_indexes = list(np.argsort(np.abs(distance)).reshape(1,-1).flatten())[0:3]
 			names = [feature_Order[i] for i in dist_indexes]
